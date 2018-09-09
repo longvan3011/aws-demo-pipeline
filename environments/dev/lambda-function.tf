fddc7ca56dee7c70dfe11a67774faaf5055baad0
EC2 Default User <ec2-user@ip-172-31-31-134.us-east-2.compute.internal>
2018-09-09 06:02:00 +0000

    updated lambda function

--- environments/dev/lambda-function.tf
+++ environments/dev/lambda-function.tf
@@ -29,7 +29,7 @@ resource "aws_lambda_function" "test_lambda" {
   function_name    = "lambda_function_name"
   role             = "${aws_iam_role.iam_for_lambda.arn}"
   handler          = "index.handler"
-  source_code_hash = "${base64sha256(file("lambda_function.zip"))}"
+  source_code_hash = "${data.archive_file.lambda_zip.output_base64sha256}"
   runtime          = "nodejs6.10"
 
   environment {
