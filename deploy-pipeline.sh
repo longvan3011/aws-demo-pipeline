3dc42517a6b55cccb9e8e3e45c8a8c30ae8348a3
Long Van <longv3011@gmail.com>
2018-09-11 11:58:07 +0000

    added java build stage

---
+++ deploy-pipeline.sh
@@ -0,0 +1,15 @@
+
+#!/bin/bash -e
+STACKNAME="aws-demo-pipeline"
+echo "Enter the GitHub OAuth Token:"
+read GITHUB_OAUTH_TOKEN
+
+
+aws cloudformation describe-stacks --stack-name $STACKNAME --query Stacks[0].StackId  > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+    echo "UPDATE $STACKNAME"
+    aws cloudformation update-stack --stack-name $STACKNAME --template-body file://deploy-pipeline.yaml --parameters  ParameterKey=GitHubOAuthToken,ParameterValue=$GITHUB_OAUTH_TOKEN  --capabilities CAPABILITY_IAM
+else
+    echo "CREATE $STACKNAME"
+    aws cloudformation create-stack --stack-name $STACKNAME --template-body file://deploy-pipeline.yaml --parameters  ParameterKey=GitHubOAuthToken,ParameterValue=$GITHUB_OAUTH_TOKEN  --capabilities CAPABILITY_IAM
+fi
